7ece076dbabeeb27883fe9ee4f0e1d76063fb904
Andre Marcio <andre.marcio.ti@gmail.com>
2026-04-29 23:24:25 -0300

    verify task

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
 def main():
-    # write your code here
-    pass
+    name_base = input("policy")
+    name_file = f"{name_base}.txt"
+    # ... collect content ...
+
+    content = []
+
+    print("Digite o conteúdo (digite 'stop' para finalizar")
+    while True:
+        linha = input("Digite a próxima linha do conteúdo:")
+        if linha == "stop":
+            break
+        content.append(linha)
+    with open(name_file, "w") as file:
+        file.write("\n".join(content))
 
 
 if __name__ == "__main__":
