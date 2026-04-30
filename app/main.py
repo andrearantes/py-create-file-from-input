262d0081b089d8afece69cf40a886340b29703d2
Andre Marcio <andre.marcio.ti@gmail.com>
2026-04-29 23:40:08 -0300

    verify task

--- app/main.py
+++ app/main.py
@@ -1,11 +1,8 @@
 def main() -> None:
     name_base = input("policy")
     name_file = f"{name_base}.txt"
-    # ... collect content ...
-
     content = []
-
-    print("Digite o conteúdo (digite 'stop' para finalizar")
+    print("Digite o conteúdo (digite 'stop' para finalizar)")
     while True:
         linha = input("Digite a próxima linha do conteúdo:")
         if linha == "stop":
@@ -13,7 +10,6 @@ def main() -> None:
         content.append(linha)
     with open(name_file, "w") as file:
         file.write("\n".join(content))
-
 
 if __name__ == "__main__":
     main()
